bd46616b19ea1c5b9646dcf98889ebd178bc33bd
CrisAndes <gonzalomorresi@gmail.com>
2024-04-11 18:12:40 -0300

    Se incorpora vista de login usando UI Kitten para algunos estilos e iconos.

--- src/presentation/screens/auth/LoginScreen.tsx
+++ src/presentation/screens/auth/LoginScreen.tsx
@@ -1,13 +1,84 @@
-import { Text, View } from 'react-native'
+import { Layout, Text, Input, Button } from "@ui-kitten/components"
+import { useWindowDimensions } from "react-native"
+import { ScrollView } from "react-native-gesture-handler"
+import { MyIcon } from "../../components/ui/MyIcon";
 
 
-export const LoginScreen = ()=> {
- 
-    return (
-      <View>
-        <Text>LoginScreen</Text>
-      </View>
-    )
-  
+
+
+export const LoginScreen = () => {
+
+  const { height } = useWindowDimensions();
+
+  return (
+    <Layout style={{ flex: 1 }}>
+      <ScrollView style={{ marginHorizontal: 40 }}>
+        <Layout style={{ paddingTop: height * 0.35 }}>
+          <Text category="h1"
+          style={{ marginBottom: 20 }}
+          > Ingresar </Text>
+          <Text category="p2"> Por favor, ingrese para continuar</Text>
+        </Layout>
+
+        {/* Inputs */}
+
+        <Layout style={{ marginTop: 20 }}>
+          <Input
+            placeholder="correo electronico"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            accessoryLeft={ <MyIcon name="email-outline" />}
+            style={{ marginBottom: 10 }}
+          />
+          <Input
+            placeholder="contraseña"
+            autoCapitalize="none"
+            secureTextEntry
+            accessoryLeft={ <MyIcon name="lock-outline" />}
+            style={{ marginBottom: 10 }}
+          />
+        </Layout>
+
+        {/* Espacio */}
+
+        <Layout style={{ height: 10 }} />
+
+        {/* Button */}
+
+        <Layout style={{ marginTop: 20 }}>
+          <Button
+          accessoryRight={ <MyIcon name="arrow-forward-outline" white /> }
+            onPress={() => { }}
+          >
+            Ingresar
+          </Button>
+
+        </Layout>
+
+        {/* informacion para crear cuenta */}
+        <Layout style={{ height: 50 }} />
+
+        <Layout style={{
+          alignItems: 'flex-end',
+          flexDirection:'row',
+          justifyContent:'center'
+        }}>
+          <Text>
+            ¿no tienes cuenta?
+          </Text>
+          <Text status="primary"
+           category="s1"
+           onPress={()=> {}}
+           >
+            {' '}
+            Crear cuenta{' '}
+          </Text>
+        </Layout>
+
+      </ScrollView>
+
+    </Layout>
+  )
+
 }
 
